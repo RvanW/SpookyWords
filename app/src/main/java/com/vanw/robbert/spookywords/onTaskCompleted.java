1f303ab60d62503b61fc3b7e9f08eaa894e6db18
Robbert <robbertvanwaardhuizen@gmail.com>
2015-10-15 22:19:03 +0200

    Fixed a lot of bugs.. better code quality (i hope)

---
+++ app/src/main/java/com/vanw/robbert/spookywords/onTaskCompleted.java
@@ -0,0 +1,11 @@
+package com.vanw.robbert.spookywords;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Robbert van Waardhuizen on 14-10-2015.
+ * Student number: 10543147
+ */
+public interface OnTaskCompleted{
+    void onTaskCompleted(ArrayList<Game> result);
+}
